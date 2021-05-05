b7f0e1955b97414f25b65bb4aad9eda20129c050
Dieg Luna <diegofcolunalopez@gmail.com>
2021-05-05 11:35:04 -0500

    mplementar y probar las conexiones

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import router from "./routes";
 
 // esperamos a que pase el "load", y queremos que aga algo
 window.addEventListener("load", router);
+
+window.addEventListener("hashchange", router);
