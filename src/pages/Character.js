bf1b19aae5576bb94d0fd412afeb9e22a16c4caa
Dieg Luna <diegofcolunalopez@gmail.com>
2021-05-05 10:49:52 -0500

    Crear template de personajes

---
+++ src/pages/Character.js
@@ -0,0 +1,35 @@
+const Characters = () => {
+  const view = `
+  <div class="Characters-inner" >
+    <article class="Characters-card" >
+        <img src="image" alt="Nombre del personaje" />
+        <h2>Name</h2>
+    </article>
+    <article class="Characters-card" >
+        <h3>
+            Episodes:
+        </h3>
+        <h3>
+            status:
+        </h3>
+        <h3>
+            Species:
+        </h3>
+        <h3>
+            Gender:
+        </h3>
+        <h3>
+            Origin:
+        </h3>
+        <h3>
+            Last location::
+        </h3>
+
+    </article>
+  </div>
+  `;
+
+  return view;
+};
+
+export default Characters;
