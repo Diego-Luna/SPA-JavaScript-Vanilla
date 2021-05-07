e9a2e221aa82556d029864a3c7f046801dbb1999
Dieg Luna <diegofcolunalopez@gmail.com>
2021-05-07 11:39:23 -0500

    deploy

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import router from "./routes";
-
-// esperamos a que pase el "load", y queremos que aga algo
-window.addEventListener("load", router);
-
-window.addEventListener("hashchange", router);
