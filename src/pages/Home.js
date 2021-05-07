b8f4638fb834cad8eeb75277a4b948478a8969f5
Dieg Luna <diegofcolunalopez@gmail.com>
2021-05-07 10:55:14 -0500

    Obtener personajes,función de llamado a la API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
-const Home = () => {
+import getData from "../utils/getData";
+
+const Home = async () => {
+  const characters = await getData();
+
   const view = `
     <div class="Characters" >
-        <article class="Characters-item" >
-        <a href="#/1/">
-            <img src="image" alt="name" />
-            <h2>Name</h2>
+
+    ${characters.results
+      .map(
+        (character) => `
+      <article class="Characters-item" >
+        <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}" />
+            <h2>${character.name}</h2>
         </a>
-        </article>
+      </article>
+    `
+      )
+      .join("")}
     </div>
   `;
 
